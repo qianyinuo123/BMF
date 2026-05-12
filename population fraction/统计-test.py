5fb1d7bf68478752ac106b522c750169eb396b98
qianyinuo <qianyinuo123@163.com>
2026-05-12 22:34:04 +0900

    data

--- population fraction/统计-test.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-# 读取xlsx文件
-df = pd.read_excel('Angola-2023.xlsx', header=None)  # header=None 确保不将第一行作为列名
-
-# 读取第4列第23行 (索引从0开始，所以是第3列第22行)
-value = df.iloc[22, 3]  # iloc[行索引, 列索引]
-print(value)
